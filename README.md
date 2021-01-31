99fd66214be31443b48c1af0ef04d001333aa38b
haolipeng <lipeng.hao@colasoft.com.cn>
2021-01-31 11:31:44 +0800

    1、为cli命令行参数添加注释

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # groupcache_db_experiment
 groupcache实验代码
 
-使用方法：
-1、
+组件介绍
+1、slowdb
+模仿慢速数据库操作
+写入操作：瞬时写入
+读取操作：300ms延迟
+
+
--- cli/cli.go
+++ cli/cli.go
@@ -11,15 +11,16 @@ import (
 func main() {
 
 	var port = flag.String("port", "9001", "frontend port")
-	var set = flag.Bool("set", false, "doing a set?")
+	var set = flag.Bool("set", true, "doing a set?")
 	var get = flag.Bool("get", false, "doing a get?")
-	var cget = flag.Bool("cget", false, "doing a get?")
+	var cacheGet = flag.Bool("cget", false, "doing a cache get?")
 	var key = flag.String("key", "foo", "key to get")
 	var value = flag.String("value", "bar", "value to set")
 	flag.Parse()
 
 	client := new(client.Client)
-	if *cget {
+	//cacheGet是通过rpc获取cache缓存内容
+	if *cacheGet {
 		client, err := rpc.DialHTTP("tcp", "localhost:"+*port)
 		if err != nil {
 			fmt.Printf("error %s", err)
@@ -34,12 +35,14 @@ func main() {
 		return
 	}
 
+	//get为数据库读操作
 	if *get {
 		var reply = client.Get(*key)
 		fmt.Println(reply)
 		return
 	}
 
+	//set为数据库写操作
 	if *set {
 		client.Set(*key, *value)
 		return
